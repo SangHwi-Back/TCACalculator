ce29cc929d1e60e2f9f8c35c25cc8a683f2c82f8
백상휘 <sanghwiback@gmail.com>
2023-12-15 16:13:48 +0900

    Initial Commit

---
+++ TCACalculator/TCACalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  TCACalculatorApp.swift
+//  TCACalculator
+//
+//  Created by 백상휘 on 2023/12/15.
+//
+
+import SwiftUI
+
+@main
+struct TCACalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
